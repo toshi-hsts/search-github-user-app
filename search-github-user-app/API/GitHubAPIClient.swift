20eda8a21f618174aae79d3a4a728a3ddca39095
toshi <tab.hsts@gmail.com>
2023-05-19 20:01:15 +0900

    詳細画面表示

--- search-github-user-app/API/GitHubAPIClient.swift
+++ search-github-user-app/API/GitHubAPIClient.swift
@@ -59,8 +59,6 @@ class GitHubAPIClient {
             fatalError()
         }
         
-        print(urlString)
-        
         let authorization = Env.accessToken.isEmpty ? "" : "Bearer \(Env.accessToken)"
         var request = URLRequest(url: url)
         request.httpMethod = "GET"
@@ -95,5 +93,49 @@ class GitHubAPIClient {
         }
         
         return user
+    }
+    
+    func getRepositories(with userName: String) async throws -> [Repository] {
+        let urlString = "https://api.github.com/users/\(userName)/repos"
+        var repositories: [Repository] = []
+        
+        guard let url = URL(string: urlString) else {
+            fatalError()
+        }
+        
+        let authorization = Env.accessToken.isEmpty ? "" : "Bearer \(Env.accessToken)"
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        // ヘッダ情報付与
+        request.allHTTPHeaderFields = [
+            "X-GitHub-Api-Version" : "2022-11-28",
+            "Accept" : "application/vnd.github+json",
+            "Authorization" : authorization
+        ]
+        
+        do {
+            let (data, urlResponse) = try await URLSession.shared.data(for: request)
+            
+            guard let httpStatus = urlResponse as? HTTPURLResponse else {
+                fatalError()
+            }
+            
+            switch httpStatus.statusCode {
+            case 200 ..< 400:
+                print("koko")
+                let searchResult  = try JSONDecoder().decode(RepositorySearchResult.self, from: data)
+                print(searchResult)
+                repositories = searchResult.repositories
+            case 400... :
+                fatalError()
+            default:
+                fatalError()
+                break
+            }
+        } catch {
+            fatalError()
+        }
+        
+        return repositories
     }
 }
--- search-github-user-app/UserDetailViewController.swift
+++ search-github-user-app/UserDetailViewController.swift
@@ -11,8 +11,15 @@ class UserDetailViewController: UIViewController {
     var userWrapper: UserWrapper?
     let githubAPIClient = GitHubAPIClient()
     var user: User?
+    var repositories: [Repository] = []
     
-    
+    @IBOutlet weak var iconImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var fullNameLabel: UILabel!
+    @IBOutlet weak var followerLabel: UILabel!
+    @IBOutlet weak var followingLabel: UILabel!
+    @IBOutlet weak var bioLabel: UILabel!
+    @IBOutlet weak var repositoryTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +28,40 @@ class UserDetailViewController: UIViewController {
         
         Task {
             user = try await githubAPIClient.getUser(with: userWrapper.name)
+            repositories = try await githubAPIClient.getRepositories(with: userWrapper.name)
+            await setInfo()
         }
     }
+    
+    private func setInfo() async {
+        guard let user else { return }
+        
+        iconImageView.setImage(with: URL(string: user.avatarUrl))
+        nameLabel.text = user.name
+        fullNameLabel.text = user.fullName
+        followerLabel.text = "follwer数：　\(user.followers)"
+        followingLabel.text = "follwing数：　\(user.following)"
+        bioLabel.text = user.bio
+        
+        repositoryTableView.reloadData()
+    }
+}
+
+// MARK: - UITableViewDataSource
+extension UserDetailViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return repositories.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "repoCell", for: indexPath)
+        
+        cell.textLabel?.text = repositories[indexPath.row].name
+        
+        return cell
+    }
+}
+
+// MARK: - UITableViewDelegate
+extension UserDetailViewController: UITableViewDelegate {
 }
